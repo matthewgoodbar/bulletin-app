52d7c0cb2b3638d8d40bac3d518660de2da0eddf
matthew <matthewgoodbar@gmail.com>
2023-03-23 16:26:37 -0700

    Add user registration, make db entrypoint async instead of callback

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,15 +1,26 @@
-const { Pool, Pool } = require('pg');
-const dbLog = requre('debug')('backend:postgres');
+const { Pool } = require('pg');
+const dbLog = require('debug')('backend:postgres');
 
 const pool = new Pool();
 
+// module.exports = {
+//     query: (text, params, callback) => {
+//         const start = Date.now();
+//         return pool.query(text, params, (err, res) => {
+//             const duration = Date.now() - start;
+//             dbLog('executed query', { text, duration, rows: res.rowCount });
+//             callback(err, res);
+//         });
+//     },
+// }
+
 module.exports = {
-    query: (text, params, callback) => {
+    async query(text, params) {
         const start = Date.now();
-        return pool.query(text, params, (err, res) => {
-            const duration = Date.now() - start;
-            dbLog('executed query', { text, duration, rows: res.rowCount });
-            callback(err, res);
-        });
+        const res = await pool.query(text, params);
+        const duration = Date.now() - start;
+        dbLog('executed query');
+        dbLog({ text, duration, rows: res.rowCount });
+        return res;
     },
 }
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,16 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../../db');
+const bcrypt = require('bcryptjs');
+const debug = require('debug')('backend:debug');
 
-router.get('/', (req, res, next) => {
-  res.json({
-    message: 'GET /api/users'
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const dbQuery = await db.query('SELECT id, username, "createdAt" FROM users');
+    res.json(dbQuery.rows);
+  } catch (err) {
+    return next(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  res.json({
-    request: req.body
-  });
+router.post('/register', async (req, res, next) => {
+  try {
+    // See if user exists with username
+    const dbQuery = await db.query('SELECT * FROM users WHERE username = $1', [req.body.username]);
+
+    // Username already exists
+    if (dbQuery.rows[0]) { 
+      const err = new Error('Validation Error');
+      err.statusCode = 400;
+      const errors = {};
+      errors.username = "A user has already been registered with this username";
+      err.errors = errors;
+      return next(err);
+    }
+
+    // Username does not exist, go ahead with user registration
+    const text = 'INSERT INTO users(username, "hashedPassword") VALUES($1, $2) RETURNING id, username, "createdAt"';
+    const values = [req.body.username];
+
+    // Create hashed password
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) throw err;
+      bcrypt.hash(req.body.password, salt, async (err, newHashedPassword) => {
+        if (err) throw err;
+        values.push(newHashedPassword);
+
+        // Try adding new user to database
+        try {
+          const addUserQuery = await db.query(text, values);
+          res.json(addUserQuery.rows[0])
+        } catch (err) {
+          return next(err);
+        }
+
+      });
+    });
+
+  } catch (err) {
+    return next(err);
+  }
 });
 
 module.exports = router;
