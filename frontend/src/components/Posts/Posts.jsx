1b8fb6f1812a771636754af1d9fce8743796805e
matthew <matthewgoodbar@gmail.com>
2023-06-25 11:06:22 -0700

    Posts box styling

--- frontend/src/components/Posts/Posts.jsx
+++ frontend/src/components/Posts/Posts.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { fetchPosts } from "../../store/posts";
 
 const Posts = () => {
@@ -13,19 +14,22 @@ const Posts = () => {
 
     return (
         <>
-            <ul>
-                {posts &&
-                posts.map((post) => 
-                    <li>
-                        <ul>
-                            <li>{post.id}</li>
-                            <li>{post.title}</li>
-                            <li>{post.body}</li>
-                            <li>{post.createdAt}</li>
-                        </ul>
-                    </li>
-                )}
-            </ul>
+            <div id="posts-box">
+                <ul>
+                    <Link to="/">Back to Homepage</Link>
+                    {posts &&
+                    posts.map((post) => 
+                        <li>
+                            <ul>
+                                <li>{post.id}</li>
+                                <li>{post.title}</li>
+                                <li>{post.body}</li>
+                                <li>{post.createdAt}</li>
+                            </ul>
+                        </li>
+                    )}
+                </ul>
+            </div>
         </>
     );
 };
