f5b2a1a662f12d6be47f194825ab296d8e2acaf7
matthew <matthewgoodbar@gmail.com>
2023-06-29 12:01:14 -0700

    404 page styling

--- frontend/src/components/NotFound/NotFound.jsx
+++ frontend/src/components/NotFound/NotFound.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <>
-            <h1>404: PAGE NOT FOUND</h1>
-            <p><Link to='/'>Return home</Link></p>
-        </>
+        <div className="specific-page-content not-found">
+            <h2 className="session-header-text">404: PAGE NOT FOUND</h2>
+            <Link to='/'>Return home</Link>
+        </div>
     );
 };
 
