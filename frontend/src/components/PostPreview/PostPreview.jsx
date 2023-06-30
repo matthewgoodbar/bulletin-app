5c9860f75db90356338756b5ef2b6df34d162bdf
matthew <matthewgoodbar@gmail.com>
2023-06-30 11:23:19 -0700

    Add assets folder to frontend, placeholder profile pics

--- frontend/src/components/PostPreview/PostPreview.jsx
+++ frontend/src/components/PostPreview/PostPreview.jsx
@@ -1,15 +1,21 @@
 import { partialTimestamp } from "../../utils/date";
+import profilePic from '../../assets/noimage-64.png'
 
 const PostPreview = ({ post }) => {
     return (
         <li className="single-post">
-            <div className="post-header">
-                <p>ID: {post.id}</p>
-                <p>Posted by: {post.author.username}</p>
-                <p>{partialTimestamp(post.createdAt)}</p>
+            <div className="post-preview-profile">
+                <img src={profilePic} alt="" />
+                <p>{post.author.username}</p>
             </div>
-            <p className="post-title">{post.title}</p>
-            <p className="post-body">{post.body}</p>
+            <div className="post-preview-content">
+                <div className="post-preview-header">
+                    <p>ID: {post.id}</p>
+                    <p>{partialTimestamp(post.createdAt)}</p>
+                </div>
+                <p className="post-title">{post.title}</p>
+                <p className="post-body">{post.body}</p>
+            </div>
         </li>
     )
 };
