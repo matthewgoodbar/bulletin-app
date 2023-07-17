c96c989f12a618cc7f2dd79d51fa7528cc541138
matthew <matthewgoodbar@gmail.com>
2023-07-17 16:12:50 -0700

    Fetch user in User component

--- frontend/src/components/User/User.jsx
+++ frontend/src/components/User/User.jsx
@@ -1,21 +1,31 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { clearPosts } from "../../store/posts";
+import { clearUsers, fetchUser } from "../../store/users";
 
 const User = () => {
 
     const dispatch = useDispatch();
     const { userId } = useParams();
+    const user = useSelector(state => state.users[userId]);
 
     useEffect(() => {
         dispatch(clearPosts());
+        dispatch(clearUsers());
+        dispatch(fetchUser(userId));
     }, [dispatch, userId]);
 
     return (
         <div id="form-and-box">
             <div id="user-box">
-
+                {user &&
+                <ul>
+                    <li>{user.id}</li>
+                    <li>{user.username}</li>
+                    <li>{user.bio}</li>
+                </ul>
+                }
             </div>
         </div>
     );
